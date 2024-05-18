5213915fd421939f5df5e5cf366e74f2e644ee57
Edward-Kulyk <kulikeduard89@gmail.com>
2024-05-18 15:39:37 +0200

    add: config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    SHORT_IO_API_KEY = "short io api"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///utm_links.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPERTY_IDS = ["GA4 Property id"]
+    api_key = "Google cloud api"
